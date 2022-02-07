36eccbd2fdf7e63c50e361b527fc6c9eebe06ead
sirichandanagangam <97136285+sirichandanagangam@users.noreply.github.com>
2022-02-06 19:11:15 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -21,18 +21,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let's see if we can fetch the weather from the backend.
     //
 
-    let client = reqwest::Client::new();
+    //let client = reqwest::Client::new();
 
-    let response = client
-        .get("https://api.openweathermap.org/data/2.5/weather?q=corvallis&appid=b98e3f089c86867862f28236d174368a&&units=imperial")
+
+    let client = reqwest::Client::new();
+    let p = model::User {
+        username: "Amulya".into(),
+        password: "123".into(),
+    };
+
+    let res = reqwest::Client::new()
+        .post("http://54.234.254.71:3000/v1/auth")
+        .json(&p)
         .send()
         .await?;
 
-    let weather2 = response
-        .json::<model::Weather>()
+    let js = res.json::<model::UserResponse>().await?;
+
+    // println!("{:#?}", js);
+
+    // println!("Access token: {:#?}", js.access_token);
+    let response = client
+        .get("http://54.234.254.71:3000/v1/weather")
+        .header("Authorization", "Bearer ".to_owned() + &js.access_token)
+        .send()
         .await?;
 
-    println!("\nWeather from openweathermap.org:\n {:?}", weather2);
+    let weather2 = response.json::<model::Weather>().await?;
+
+    println!(
+        "\nWeather from server running on ec2 instance:\n {:?}",
+        weather2
+    );
+
+    // fetch greeting
+    let response = client
+        .get("http://54.234.254.71:3000/v1/hello")
+        .header("Authorization", "Basic ".to_owned() + &js.access_token)
+        .send()
+        .await?;
+
+    let greeting = response.json::<model::GreetingResponse>().await?;
+
+    println!(
+        "\nGreeting from server running on ec2 instance:\n {:?}",
+        greeting.message
+    );
+
 
     Ok(())
 }
